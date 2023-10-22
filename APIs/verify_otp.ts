7c7209ba37426710410737438549d541c31c884d
Manik Mehta <mehtamanik96@gmail.com>
2023-10-22 23:35:39 +0530

    Verify OTP API Test

--- APIs/verify_otp.ts
+++ APIs/verify_otp.ts
@@ -7,10 +7,21 @@ const verifyOTP=express.Router();
 verifyOTP.post('/verify-email', authMiddlewre, async(req, res)=>{
     try{
     const {otp, otptoken}=req.body;
-    const isVerified=verifyOtp(parseInt(otp), otptoken, 'emailotp');
+    if(otp==null||otp==undefined||otptoken==null||otptoken==undefined){
+        return res.status(400).json({'msg': "OTP or token is not provided"});
+    }
+    if(otp.toString().length!=4){
+        return res.status(400).json({'msg': "Invalid OTP"});
+    }
+    let parsedOtp:number=parseInt(otp)
+    if(Number.isNaN(parsedOtp)){
+        return res.status(400).json({'msg': "Invalid OTP"})
+    }
+   
+    const isVerified=verifyOtpObject.verifyOTP(parsedOtp, otptoken, 'emailotp');
     if(isVerified){
         const user=await User.findByIdAndUpdate(res.locals.id, {isEmailVerified: true}, {new: true});
-        
+        return res.status(200).json(user)
     }
     else{
         res.status(400).json({'msg': "Verification failed"});
@@ -19,5 +30,8 @@ verifyOTP.post('/verify-email', authMiddlewre, async(req, res)=>{
         res.status(500).json({'msg': e.message});
        }
 })
-
+// this object is created for testing purposes
+export const verifyOtpObject={
+    verifyOTP:verifyOtp
+   }
 export {verifyOTP};
--- enviornment_variables.ts
+++ enviornment_variables.ts
@@ -5,3 +5,6 @@ dotenv.config()
 export const DatabaseUrl: string=process.env.DATABASE_URL!
 export const IP:string=process.env.IP_ADDRESS!
 export const FirebasePath:string=process.env.FIREBASE_PATH!
+export const NodemailerSenderEmail:string=process.env.NODEMAILER_SENDER_EMAIL!
+export const NodemailerSenderPassword:string=process.env.NODEMAILER_SENDER_PASSWORD!
+export const NodemailerService:string=process.env.NODEMAILER_SPMTP_SERVICE!
